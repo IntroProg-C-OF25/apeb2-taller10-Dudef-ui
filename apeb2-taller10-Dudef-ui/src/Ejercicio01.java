2f50c89790dad0746ec2e35291ed6a5ff462bc71
Dagonzález2006 <dudef.2222.11@gmail.com>
2024-12-22 13:24:46 -0500

    Resolucion de los ejercicios 1 y 2 (tiempo empleado=2.30h)

--- apeb2-taller10-Dudef-ui/src/Ejercicio01.java
+++ apeb2-taller10-Dudef-ui/src/Ejercicio01.java
@@ -1,7 +1,117 @@
+
+import java.util.Scanner;
+
 /**
+ * Dada una matriz cuadrada (m X m) de valores enteros aleatorios, desarrollar
+ * su procesamiento para presentar los elementos: De la diagonal principal. De
+ * la diagonal segundaria. Ubicados bajo la diagonal principal. Ubicados sobre
+ * la diagonal principal. Ubicados bajo la diagonal secundaria. Ubicados sobre
+ * la diagonal secundaria.
  *
- * @author Usuario
+ * @author David González/Dudef-ui
  */
 public class Ejercicio01 {
-    
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int extension_Matriz = 0;
+        System.out.print("Dime la extension de la matriz: ");
+        extension_Matriz = teclado.nextInt();
+        int matriz[][] = new int[extension_Matriz][extension_Matriz];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                matriz[i][j] = (int) (Math.random() * 10);
+            }
+        }
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println("");
+        }
+        System.out.println("Solucion obvia");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                if (i == j) {
+                    System.out.print(matriz[i][j] + "\t");
+                }
+            }
+        }
+        System.out.println("");
+        //ES EFICIENTE PORQUE REDUCE CODIGO Y REDUCE LA CANTIDAD DE CICLOS
+        System.out.println("Solucion eficiente: ");
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.print(matriz[i][i] + "\t");
+        }
+        System.out.println("");
+        System.out.println("Solucion secundaria obvia: ");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                if ((i + j) == matriz.length - 1) {
+                    System.out.print(matriz[i][j] + "\t");
+                }
+            }
+        }
+        System.out.println("");
+        System.out.println("Solucion secundaria eficiente: ");
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.print(matriz[i][matriz.length - i - 1] + "\t");
+        }
+        System.out.println("");
+        System.out.print("Solucion bajo la diagonal principal:\n");
+        for (int i = 1; i < matriz.length; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+        }
+        System.out.println("");
+        System.out.print("Solucion sobre la diagonal principal:\n");
+        for (int i = 0; i <= matriz.length; i++) {
+            for (int j = 1 + i; j < matriz.length; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+        }
+        System.out.println("");
+        System.out.print("Solucion bajo la diagonal secundaria:\n");
+        for (int i = 1; i < matriz.length; i++) {
+            for (int j = 1; j < matriz.length; j++) {
+                if (i + j > matriz.length - 1) {
+                    System.out.print(matriz[i][j] + "\t");
+                }
+            }
+        }
+        System.out.println("");
+        System.out.print("Solucion sobre la diagonal secundaria:\n");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                if (i + j < matriz.length - 1) {
+                    System.out.print(matriz[i][j] + "\t");
+                }
+            }
+        }
+        System.out.println("");
+    }
 }
+/***
+ * run:
+ * 2	9	8	
+ * 5	8	9	
+ * 4	4	4	
+ * Solucion obvia
+ * 2	8	4	
+ * Solucion eficiente: 
+ * 2	8	4	
+ * Solucion secundaria obvia: 
+ * 8	8	4	
+ * Solucion secundaria eficiente: 
+ * 8	8	4	
+ * Solucion bajo la diagonal principal:
+ * 5	4	4	
+ * Solucion sobre la diagonal principal:
+ * 9	8	9	
+ * Solucion bajo la diagonal secundaria:
+ * 9	4	4	
+ * Solucion sobre la diagonal secundaria:
+ * 2	9	5	
+ * BUILD SUCCESSFUL (total time: 0 seconds)
+ */
--- apeb2-taller10-Dudef-ui/src/Ejercicio02.java
+++ apeb2-taller10-Dudef-ui/src/Ejercicio02.java
@@ -1,7 +1,90 @@
+
+import java.util.Scanner;
+
 /**
+ * Dadas dos matrices (cuadradas y/o rectangulares) de valores enteros
+ * aleatorios, desarrollar su procesamiento para calcular y presentar: La suma
+ * de las dos matrices (considerar las restricciones matemáticas para ello). La
+ * multiplicación de las dos matrices (considerar las reglas matemáticas para
+ * ello).
  *
- * @author Usuario
+ * @author David González/Dudef-ui
  */
 public class Ejercicio02 {
-    
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int filas, columnas, opcion = 0;
+        //Se solicita al usuario el tamaño de las matrices
+        System.out.print("Dime cuantas filas tienen las matrices: ");
+        filas = teclado.nextInt();
+        System.out.print("Dime cuantas columnas tienen las matrices: ");
+        columnas = teclado.nextInt();
+        //Se declara las matrices:
+        int matriz1[][] = new int[filas][columnas];
+        int matriz2[][] = new int[filas][columnas];
+        int matriz3[][] = new int[filas][columnas];
+        //Se utiliza un Math.random para llenar las matrices con valores de manera automatica
+        System.out.println("Primera matriz: ");
+        for (int i = 0; i < matriz1.length; i++) {
+            for (int j = 0; j < matriz1[0].length; j++) {
+                matriz1[i][j] = (int) (Math.random() * 10);
+                System.out.print(matriz1[i][j] + "\t");
+            }
+            System.out.println("\n");
+        }
+
+        System.out.println("Segunda matriz: ");
+        for (int i = 0; i < matriz2.length; i++) {
+            for (int j = 0; j < matriz2[0].length; j++) {
+                matriz2[i][j] = (int) (Math.random() * 10);
+                System.out.print(matriz2[i][j] + "\t");
+            }
+            System.out.println("\n");
+        }
+        //Suma de las matrices
+        System.out.println("¿Que quieres hacer con las matrices? (1 = sumar, 2 = multiplicar)");
+        opcion = teclado.nextInt();
+        if (opcion == 1) {
+            System.out.println("Suma de matrices: ");
+            for (int i = 0; i < matriz1.length; i++) {
+                for (int j = 0; j < matriz1[0].length; j++) {
+                    matriz3[i][j] = matriz1[i][j] + matriz2[i][j];
+                    System.out.print(matriz3[i][j] + "\t");
+                }
+                System.out.println("");
+            }
+            //En caso de multiplicacion de matrices
+        } else if (opcion == 2) {
+            System.out.println("Multiplicacion de matrices: ");
+            for (int i = 0; i < matriz1.length; i++) {
+                for (int j = 0; j < matriz1[0].length; j++) {
+                    for (int k = 0; k < matriz1[0].length; k++) {
+                        //Signo += ya que se va a sumar la multiplicacion de ambas matrices.
+                        // matriz3[i][j] += matriz1[i][k] * matriz2[k][j];
+                        matriz3[i][j] = matriz3[i][j] + (matriz1[i][k] * matriz2[k][j]);
+                    }
+                    System.out.print(matriz3[i][j] + "\t");
+                }
+                System.out.println("");
+            }
+        }
+    }
 }
+/***
+ * run:
+ * Dime cuantas filas tienen las matrices: 2
+ * Dime cuantas columnas tienen las matrices: 2
+ * Primera matriz: 
+ * 6	4	
+ * 9	5	
+ * Segunda matriz: 
+ * 3	9	
+ * 1	6	
+ * ¿Que quieres hacer con las matrices? (1 = sumar, 2 = multiplicar)
+ * 2
+ * Multiplicacion de matrices: 
+ * 22	78	
+ * 32	111	
+ * BUILD SUCCESSFUL (total time: 11 seconds)
+ */
